49c27461a50fb757b244e79aaa2308a01be34dfb
crisfrias <TU_EMAIL>
2026-04-25 19:38:42 +0200

    Change in the implementation of desviation.c and desviation.h adapting it with the new node.h definition and making it more clear

--- desviation.c
+++ desviation.c
@@ -1,34 +1,35 @@
 #include "desviation.h"
 
-int region_in_path(int *path, int path_size, graph *all_nodes, node *region) {
+int region_in_path(int index, int *path, int path_size) {
 	int is_in_path = 0;
 	for (int i = 0; i < path_size && !is_in_path; ++i) {
-		if (all_nodes[path[i]] == *region) {
+		if (path[i] == index) {
 			is_in_path = 1;
 		}
 	}
 	return is_in_path;
 }
 
-int region_in_graph(graph *all_nodes, node *region) {
+int region_in_graph(int index, int *neighbors, int neighbors_size) {
 	int is_in_graph = 0;
-	for (int i = 0; i < all_nodes->node_len && !is_in_graph; ++i) {
-		if (all_nodes->nodes[i] == *region) {
+	for (int i = 0; i < neighbors_size && !is_in_graph; ++i) {
+		if (neighbors[i] == index) {
 			is_in_graph = 1;
 		}
 	}
+  return is_in_graph;
 }
 
-int desviation_case(int *path, int path_size, graph *all_nodes, node *region) {
-	/* FIRST CASE: The node that the user introduced is in the path*/
-	if (region_in_path(path, path_size, all_nodes, region)) {
-		return 1;
+Desviation_case correctness_of_the_node (int index, int *path, int path_size, int *neighbors, int neighbors_size) {
+  /* FIRST CASE: The node that the user introduced is in the path*/
+	if (region_in_path(index, path, path_size)) {
+		return PATH_NODE; 
 	}
 	/* SECOND CASE: The node is not in the path but it takes part in the connected component*/
-	else if (!region_in_path(path, path_size, all_nodes, region) && region_in_graph(all_nodes, region)) {
-		return 2;
+	else if (!region_in_path(index, path, path_size) && region_in_graph(index, neighbors, neighbors_size)) {
+		return NEIGHBOR_NODE;
 	}
 	/* GENERAL CASE: The node is not in the path as well as in the connected component */ 	 else {
-		return 0;
+	return INCORRECT_NODE;
 	}
 }
--- desviation.h
+++ desviation.h
@@ -1,17 +1,38 @@
+#ifndef DESVIATION_H
+#define DESVIATION_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
 #include "struct_node.h"
 
-// Pre: path is an array of the subindex of a connected component of the graph all_nodes, path_size is how many nodes are stored in the path and region is the node that the user typed previously
+typedef enum {
+    INCORRECT_NODE, // It is the value 0
+    NEIGHBOR_NODE, // It is the value 1
+    PATH_NODE     // It is the value 2
+} Desviation_case; 
+
+// Pre: Index is the ID of the user's introduced node and it has to exist, 
+// *path is the array with all the middle nodes until the final node and path_size
+// is the number of nodes in the path
 // Post: Returns 1 if region is in the path list, 0 if it is not
-int region_in_path(int *path, int path_size, graph *all_nodes, node *region);
+int region_in_path(int index, int *path, int path_size);
 
-// Pre: all_nodes is the graph of all available nodes and region is the user's typed region
+// Pre: Index is the ID of the user's introduced node and it has to exist,
+// *neighbors is the array with all the neighbors of the target node and neighbors_size
+// is the amount of neighbors that the actual node has
 // Post: It returns 1 if region is in the graph, 0 if it is not
-int region_in_graph(graph *all_nodes, node *region);
+int region_in_graph(int index, int *neighbors, int neighbors_size);
 
-// Pre: path is an array of the subindex of a connected component of the graph all_nodes, path_size is how many nodes are stored in the path and region is the node that the user typed previously
-// Post: Returns 1 if region is in the path, 2 if it is not in the path but it is in the all_nodes graph and 0 if it is not in neither
-int desviation_case(int *path, int path_size, graph *all_nodes, node *region);
+// Pre: index is the ID of the user's introduced node and it has to exist, *path
+// is the array with all the middle nodes until the final node, path_size is 
+// the amount of nodes in the path array, *neighbors is the array of the
+// neighbors of the actual node and neighbors_size is the amount of neighbors the
+// actual node has
+// Post: Returns INCORRECT_NODE if the index is neither in the neighbors and the path
+// array, PATH_NODE if index is in the path array and NEIGHBOR_NODE if it is not 
+// in the path array but it is in the neighbors array
+Desviation_case correctness_of_the_node(int index, int *path, int path_size, int *neighbors, int neighbors_size);
+
+#endif
