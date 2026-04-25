95652d7f93c1c75eebed2b690c393a1ba94fabb3
wamu_M <contact@wamu-m.com>
2026-04-26 01:03:43 +0200

    Made auto_completion very insensitive

--- auto_completion.h
+++ auto_completion.h
@@ -3,4 +3,5 @@
 
 #define MAX_NUM_SUGGEST 3
 
-void generate_suggestions(char* input, graph* g, char** output);
+// Returns the number of suggestions that have been placed in output
+int generate_suggestions(char* input, graph* g, char** output, int* state);
