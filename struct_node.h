44c49caee1dd8261919bb3d73db4b42385bfae1a
wamu_M <contact@wamu-m.com>
2026-04-25 11:21:41 +0200

    Add node and graph def

---
+++ struct_node.h
@@ -0,0 +1,16 @@
+typedef struct node node;
+struct node 
+{
+    float lats[];
+    float lons[];
+    int len;
+    int incidence_list[];
+};
+
+typedef struct graph graph;
+
+struct graph
+{
+    node nodes[];
+    int node_len;
+}
